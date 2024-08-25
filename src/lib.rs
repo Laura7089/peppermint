4ad9e53a9a983ff8ed742249043de684ea6c9bd9
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-08-25 13:19:58 +0100

    feat: add string values to label/jumplabel

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,20 @@ pub mod lex {
         #[regex(r"[;#][^\n]+")]
         Comment,
         #[regex(r"\[[0-9]+\]", |lex| decimal(debracket(lex.slice())).ok())]
-        #[regex(r"\[0x[0-9]+\]", |lex| hexadecimal(debracket(lex.slice())).ok())]
+        #[regex(r"\[0x[0-9a-zA-Z]+\]", |lex| hexadecimal(debracket(lex.slice())).ok())]
         #[regex(r"\[0b[01]+\]", |lex| binary(debracket(lex.slice())).ok())]
         Address(Word),
         #[regex(r"[0-9]+", |lex| decimal(lex.slice()).ok())]
-        #[regex(r"0x[0-9]+", |lex| hexadecimal(lex.slice()).ok())]
+        #[regex(r"0x[0-9a-zA-Z]+", |lex| hexadecimal(lex.slice()).ok())]
         #[regex(r"0b[01]+", |lex| binary(lex.slice()).ok())]
         Literal(Word),
-        #[regex(r":[a-zA-Z][a-zA-Z_-]*")]
-        Label,
-        #[regex(r"[a-zA-Z][a-zA-Z_-]*:")]
-        JumpLabel,
+        #[regex(r":[a-zA-Z][a-zA-Z_-]*", |lex| lex.slice()[1..].to_string())]
+        Label(String),
+        #[regex(r"[a-zA-Z][a-zA-Z_-]*:", |lex| {
+            let slice = lex.slice();
+            slice[0..(slice.len() - 1)].to_string()
+        })]
+        JumpLabel(String),
     }
 
     #[derive(Debug, Clone, strum::EnumString, PartialEq, Eq)]
@@ -66,8 +69,14 @@ pub mod lex {
         use super::*;
         use test_case::test_case;
 
-        #[test_case("load" => Token::Instruction(InstructionKind::Load); "instruction")]
-        #[test_case("[0x10]" => Token::Address(16); "hex address")]
+        #[test_case("load" => Token::Instruction(InstructionKind::Load))]
+        #[test_case("[0x10]" => Token::Address(16))]
+        #[test_case("[12]" => Token::Address(12))]
+        #[test_case("[0b10]" => Token::Address(2))]
+        #[test_case("0x1A" => Token::Literal(26))]
+        #[test_case("0x2a" => Token::Literal(42))]
+        #[test_case("0b1110" => Token::Literal(14))]
+        #[test_case("1120" => Token::Literal(1120))]
         fn test_single_token(input: &str) -> Token {
             let mut lexer = Token::lexer(input);
             lexer.next().expect("no output").expect("parse error")
