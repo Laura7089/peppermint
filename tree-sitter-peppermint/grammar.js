1695bc512fde1b69bd662143586c68d08b755ddc
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-08-25 20:35:28 +0100

    feat(treesitter): basic grammar, tests

--- tree-sitter-peppermint/grammar.js
+++ tree-sitter-peppermint/grammar.js
@@ -2,10 +2,45 @@
 // @ts-check
 
 module.exports = grammar({
-  name: 'YOUR_LANGUAGE_NAME',
+  name: 'peppermint',
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => 'hello'
+    source_file: $ => repeat($._object),
+
+    _object: $ => choice(
+      $.statement,
+      $._whitespace,
+      $.comment,
+    ),
+
+    _whitespace: $ => /[ \t\n]+/,
+
+    statement: $ => choice(
+      $.instruction,
+      $.literal,
+    ),
+
+    comment: $ => seq(choice(';', '#'), $._comment_text),
+    _comment_text: $ => /[^\n]*/,
+
+    instruction: $ => seq(
+      $.opcode,
+      $._whitespace,
+      $.operand,
+    ),
+
+    opcode: $ => /[a-zA-Z]+/,
+    operand: $ => choice(
+      $.label_jump,
+      $.address,
+    ),
+
+    literal: $ => $._number,
+    address: $ => seq('[', $._number, ']'),
+    _number: $ => /(0[xb])?[0-9a-fA-F]+/,
+
+    label: $ => seq($._label_name, ':'),
+    label_jump: $ => seq(':', $._label_name),
+    _label_name: $ => /[a-zA-Z][a-zA-Z0-9\-_]+/,
   }
 });
