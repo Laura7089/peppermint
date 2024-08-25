f7eea6fd3bfbd72c9dcc1a50fa28c6e9207137bf
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-08-25 14:00:46 +0100

    feat: simple CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use logos::Logos;
 
+#[derive(Parser)]
+struct Opt {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+    Tokenise { input: PathBuf },
+    Parse { input: PathBuf },
+    Assemble { input: PathBuf },
+}
+
 fn main() {
-    let in_file = std::env::args().nth(1).expect("need an input file");
-    let content = std::fs::read_to_string(in_file).expect("couldn't read file");
+    let opt = Opt::parse();
 
-    let tokens = oshug_assembler::lex::Token::lexer(&content)
+    match opt.command {
+        Command::Tokenise { input } => {
+            let content = std::fs::read_to_string(input).expect("couldn't read file");
+            println!("{:?}", tokenize(&content));
+        }
+        Command::Parse { input } => {
+            let content = std::fs::read_to_string(input).expect("couldn't read file");
+            let mut tokens = tokenize(&content).into_iter();
+            let program = oshug_assembler::LabelledStatement::consume_token_stream(&mut tokens);
+            println!("{:?}", program);
+        }
+        Command::Assemble { input: _ } => todo!("assembler not implemented yet"),
+    }
+}
+
+fn tokenize(input: &str) -> Vec<oshug_assembler::lex::Token> {
+    oshug_assembler::lex::Token::lexer(input)
         .collect::<Result<Vec<_>, _>>()
-        .expect("parse error");
-
-    let mut tokens_iter = tokens.into_iter();
-    let program = oshug_assembler::LabelledStatement::consume_token_stream(&mut tokens_iter);
-
-    println!("{program:?}");
+        .expect("parse error")
 }
