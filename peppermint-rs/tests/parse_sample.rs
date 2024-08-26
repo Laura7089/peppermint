014d9b07243673ba814e41f009e547db32e38656
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-08-26 13:25:17 +0100

    fix(lib): make full sample parse test panic correctly

--- peppermint-rs/tests/parse_sample.rs
+++ peppermint-rs/tests/parse_sample.rs
@@ -1,5 +1,8 @@
 #[test]
 fn parse_sample_file() {
     let sample_string = std::fs::read_to_string("../sample_program.ppr").unwrap();
-    println!("{:?}", peppermint::parse_final(&sample_string));
+    println!(
+        "{:?}",
+        peppermint::parse_final(&sample_string).expect("parse error")
+    );
 }
